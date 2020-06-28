778ab886d6034fd8594e37da03965a2eaad867bb
gwangyeol-park <samjjng01@gmail.com>
2020-06-28 22:09:43 +0900

    issue #6 calculate only number

--- calc/calc.py
+++ calc/calc.py
@@ -6,11 +6,11 @@ def application(environ, start_response):
     first_num = d.get('first_num', [''])[0]
     second_num = d.get('second_num', [''])[0]
     sum, mul = 0, 0
-    try:
+    if first_num.isdigit() and second_num.isdigit():
         first_num, second_num = int(first_num), int(second_num)
     	sum = first_num + second_num
     	mul = first_num * second_num
-    except ValueError:
+    else:
 	sum = "Error"
 	mul = "Error"
     response_body = html % {'sum' : sum, 'mul' : mul}
